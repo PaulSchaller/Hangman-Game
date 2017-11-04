e76191b0c7da028bf326018dc2a00a1b8afceabc
unknown <paul_schaller@att.net>
2017-11-04 14:34:44 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,74 @@
-var colors = ["blue", "red", "purple", "black", "pink", "green", "gray", "orange", "yellow"];
+var colors = ["blue", "red", "purple", "pink", "green", "orange", "yellow", "gray"];
 var word = colors[Math.floor(Math.random()*colors.length)];
+console.log(word);
 var guessesLeft = 10;
 var answerArray = []; 
-var text;
 var guesses = [];
-console.log("helol")
+var congratulations = "Congratulations";
+console.log("hello");
+
+function initializeGame() {
+	word = colors[Math.floor(Math.random()*colors.length)]; 
+	guesses = [];
+	guessesLeft = 10;
+
+	updateAnswerArray();
+
+	updateGuesesLeft();
+}
+
 function updateAnswerArray(){
 	answerArray = [];
-		for (var i = 0; i < word.length; i++){
-			var wordLetter = word[i];
-			if (guesses.includes(wordLetter)){
-				answerArray.push(wordLetter);
-			}
-			else {
-				answerArray.push("_");
-			}
-			// text = word.split("").join(" ");
-			// console.log(text);
+	for (var i = 0; i < word.length; i++){
+		var wordLetter = word[i];
+		if (guesses.includes(wordLetter)){
+			answerArray.push(wordLetter);
 		}
+		else {
+			answerArray.push("_");
+		}
+	}
+	document.getElementById("answer").innerHTML = answerArray.join(" ");
+
 
 }
 
+function updateGuesesLeft(){
+	document.getElementById("counter").innerHTML = "Guess Left " + guessesLeft;
+
+}
+
+initializeGame();
+
 document.onkeyup = function(event){
-		var letterInput = String.fromCharCode(event.which).toLowerCase();
-		updateAnswerArray();
-		console.log(guesses, word, answerArray.join(" "))
-		if (answerArray.join("") === word){
+	var letterInput = String.fromCharCode(event.which).toLowerCase();
+	console.log(letterInput);
+	
+
+	console.log(guesses, word, answerArray.join(""))
+
+	if (!guesses.includes(letterInput)){
+		guesses.push(letterInput);
+		if (!word.includes(letterInput)){
+			guessesLeft--;
+			updateGuesesLeft();
+
+			console.log(guessesLeft)
+		} else {
+			updateAnswerArray();
+
+		}
+	}
+	if (answerArray.join("") === word){
 		document.getElementById("congratulations").innerHTML = "Congratulations.  You won.";
-		// 					//document.body.style.backgroundColor = "word";
-		}
-		if (!guesses.includes(letterInput)){
-			guesses.push(letterInput);
-			if (!word.includes(letterInput)){
-				guessesLeft--;
-				document.getElementById("counter").innerHTML = "Guess Left " + guessesLeft;
-			} else {
-				updateAnswerArray();
-				document.getElementById("answer").innerHTML = answerArray.join(" ");
+		initializeGame();
+	}
+	else if (guessesLeft == 0){
+		document.getElementById("lose").innerHTML = "Sorry.  You lose.  You are out of guesses.";
+		initializeGame();
+	}	
 
-			}
-		}
-		// for (var i = 0; i < word.length; i++) {
+	
 
-		// 	if (word[i] == letterInput){
-		// 		answerArray[i] = letterInput;;
 
-		// 		
-		// 				}
-		// 			}
-		// 			guessesLeft--;	
-		// 			document.getElementById("answer").innerHTML = answerArray.join(" ");
-
-		// 			if (guessesLeft == 0){
-		// 				document.getElementById("counter").innerHTML = "You lose the game.";
-		// 				game();
-		// 			}
-
-		// 		}
-		};
+};
